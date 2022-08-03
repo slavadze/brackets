3f7fc74054bae7733e1c414b6a6b26b96796b68f
slavadze <6354045@mail.ru>
2022-08-03 22:29:39 +0300

    feat: add if

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.forEach(element => {
+      if (str[i] === element[0] && str[i + 1] === element[1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+        i -= 2;
+      }
+    });
+  }
+  return str.length === 0 ? true : false;
 }
+
+console.log(module.exports('()', [['(', ')']])) // -> true
+console.log(module.exports('((()))()', [['(', ')']])) // -> true
